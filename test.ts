0a139543f1ea722aaf7537afca2562e701df9d9a
kishore <kumar.kishore92@gmail.com>
2025-08-23 19:23:06 +0530

    test

--- test.ts
+++ test.ts
@@ -1,6 +1,1 @@
-console.log('test');
-console.log('test');
-
-console.log('test test2 kishore fullstacks did the changes');
-
-console.log("I am kishore full stacks");
+testing done by here kishore sah
